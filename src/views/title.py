9f44b7cd961d22d7a6351061b8d517c4770c5915
Janchorizo <jancho@usal.es>
2021-02-06 21:12:52 +0100

    Change padding in title.

--- src/views/title.py
+++ src/views/title.py
@@ -3,10 +3,8 @@ import dash_html_components as html
 
 
 def title():
-    content = html.Div(className='cell my-3 shrink header medium-cell-block-container text-primary', children=[
-        html.H1(children=[
-            'Explorador del BOE',
-        ]),
+    content = html.Div(className='cell ps-5 my-3 shrink header medium-cell-block-container text-primary', children=[
+        html.H1(children='Explorador del BOE'),
         html.H2(children='Un resumen gráfico del Boletín Oficial del Estado'),
     ])
     return content
